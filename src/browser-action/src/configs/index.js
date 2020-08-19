117362ddf63e6780ed91eed58b7692816b082166
Ernest Chang <ernestc@cqcp.com.tw>
2020-08-19 18:54:07 +0800

    watch changes from different tabs & extract configs

---
+++ src/browser-action/src/configs/index.js
@@ -0,0 +1,5 @@
+export const STORAGE_KEYS = {
+  paddingColor: 'paddingColor',
+  marginColor: 'marginColor',
+  coverColor: 'coverColor',
+}
--- src/browser-action/src/views/color/index.vue
+++ src/browser-action/src/views/color/index.vue
@@ -11,6 +11,7 @@
 
 <script>
 import ColorPicker from './color-picker'
+import { STORAGE_KEYS } from '../../configs/index'
 
 export default {
   name: 'color',
@@ -27,25 +28,39 @@ export default {
 
   watch: {
     paddingColor() {
-      chrome.storage.sync.set({ paddingColor: this.paddingColor })
+      chrome.storage.sync.set({ [STORAGE_KEYS.paddingColor]: this.paddingColor })
     },
     marginColor() {
-      chrome.storage.sync.set({ marginColor: this.marginColor })
+      chrome.storage.sync.set({ [STORAGE_KEYS.marginColor]: this.marginColor })
     },
     coverColor() {
-      chrome.storage.sync.set({ coverColor: this.coverColor })
+      chrome.storage.sync.set({ [STORAGE_KEYS.coverColor]: this.coverColor })
     },
   },
 
   created() {
+    chrome.storage.sync.onChanged.addListener(this.handleStorageChange)
+
     chrome.storage.sync.get(
-      ['paddingColor', 'marginColor', 'coverColor'],
+      [STORAGE_KEYS.paddingColor, STORAGE_KEYS.marginColor, STORAGE_KEYS.coverColor],
       ({ paddingColor, marginColor, coverColor }) => {
         this.paddingColor = paddingColor
         this.marginColor = marginColor
         this.coverColor = coverColor
       },
     )
+  },
+
+  beforeDestroy() {
+    chrome.storage.sync.onChanged.removeListener(this.handleStorageChange)
+  },
+
+  methods: {
+    handleStorageChange(changes) {
+      if (changes[STORAGE_KEYS.paddingColor]) this.paddingColor = changes[STORAGE_KEYS.paddingColor].newValue
+      if (changes[STORAGE_KEYS.marginColor]) this.marginColor = changes[STORAGE_KEYS.marginColor].newValue
+      if (changes[STORAGE_KEYS.coverColor]) this.coverColor = changes[STORAGE_KEYS.coverColor].newValue
+    },
   },
 }
 </script>
