c48f6faae1461452413882e81f2cf37173da08c9
Ernest Chang <iattempt.net@gmail.com>
2020-08-20 20:41:48 +0800

    change ui and add mode option

---
+++ src/browser-action/src/views/mode/index.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+  v-list
+    v-subheader Mode
+    v-list-item
+      v-radio-group.pl-3.mt-1( v-model="mode" hide-details="auto" )
+        v-radio( label="Inspect by content" value="by-content" )
+        v-radio( label="Inspect by others" value="by-others" )
+</template>
+
+<script>
+export default {
+  name: 'mode',
+
+  data: () => ({
+    mode: 'by-content',
+  }),
+
+  watch: {
+    mode() {
+      console.log(this.mode)
+      chrome.storage.sync.set({ mode: this.mode })
+    },
+  },
+
+  created() {
+    chrome.storage.sync.onChanged.addListener(this.handleStorageChange)
+
+    chrome.storage.sync.get(
+      ['mode'],
+      ({ mode }) => {
+        this.mode = mode
+      },
+    )
+  },
+
+  beforeDestroy() {
+    chrome.storage.sync.onChanged.removeListener(this.handleStorageChange)
+  },
+
+  methods: {
+    handleStorageChange(changes) {
+      if (changes.mode) this.mode = changes.mode.newValue
+    },
+  },
+}
+</script>
